6074148840562ab0250b920f8470c7d03b1b09a9
Lada <vladislava.oblogina@gmail.com>
2026-03-21 15:57:31 +0300

    Initial project setup with Maven, JUnit, Rest Assured

---
+++ src/test/java/RestAssuredTest.java
@@ -0,0 +1,16 @@
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class RestAssuredTest {
+
+    @Test
+    void simpleGetTest() {
+        Response response = given()
+                .when()
+                .get("https://postman-echo.com/get");
+
+        System.out.println(response.getStatusCode());
+    }
+}
