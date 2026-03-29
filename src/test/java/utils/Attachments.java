50617ce673b3acdf592a6131101f263b594180e5
Lada <vladislava.oblogina@gmail.com>
2026-03-29 17:48:46 +0300

    lesson_10

---
+++ src/test/java/utils/Attachments.java
@@ -0,0 +1,14 @@
+package utils;
+
+import io.qameta.allure.Attachment;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+    public class Attachments {
+
+        @Attachment(value = "Screenshot", type = "image/png")
+        public static byte[] saveScreenshot(WebDriver driver) {
+            return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+        }
+    }
